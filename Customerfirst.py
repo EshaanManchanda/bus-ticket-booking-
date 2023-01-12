b32b5a849e38195e5ba7132f7c5d553edc1bcce2
Eshaan <eshaanmanchanda01@gmail.com>
2023-01-12 14:46:14 +0530

    first page complete

--- Customerfirst.py
+++ Customerfirst.py
@@ -147,17 +147,27 @@ class customerclass:
         self.tx8.place(x=450, y=265)
         # Meal
         # self.frame5=Frame(self.root,bd=10,relief="raised",width=350,height=280).place(x=380,y=420)
-        self.meals.set(" ")
+        self.meals.set(None)
         self.lb11 = Label(self.frame4, text="Would you like Something?",
                           width=22, font=10).place(x=380, y=440)
 
         self.meal = Radiobutton(self.frame4, text="Want Meal", font=10,
-                                variable=self.meals, value="Yes").place(x=510, y=470)
+                                variable=self.meals, value=1)
+        self.meal.place(x=510, y=470)
         self.nomeal = Radiobutton(self.frame4, text="No Meal", font=10,
-                                  variable=self.meals, value="No").place(x=510, y=500)
+                                  variable=self.meals, value=0)
+        self.nomeal.place(x=510, y=500)
         
+        self.lb12= Label(self.frame4, text="Number of Meals:",
+                            font=20, width=15)
+        self.lb12.place(x=380, y=535)
+
+        self.tx5= Entry(self.frame4, width=20, font=20,
+                            textvariable=self.feed)
+        self.tx5.place(x=450, y=576)
         self.bt = Button(self.frame4, text="Submit",command=self.submit, bd=10,
                          relief="raised", font=10).place(x=500, y=620)
+        
         self.update_content()
 
         # Reset
@@ -172,96 +182,13 @@ class customerclass:
             self.old.set("0")
             self.feed.set("")
             self.from_From.set("Select Destination")
-            self.passe.set("")
+            # self.passe.set("")
+            print(self.meals.get())
             self.to_To.set("Select Destination")
-        # ---------------------------------------------------------------------------------
-
-        # 2nd Module
-        # def new():
-        #     if num1.get()=="" and num2.get()=="" and num3.get()=="" and num4.get()=="" and passe.get()=="" and feed.get()=="":
-        #         messagebox.showerror("Error","Check all Credentials")
-        #     else:
-        #         messagebox.showinfo("Error","Welcome to Fare Page")
-
-        #         root=Tk()
-        #         root.geometry("800x750")
-        #         root.title("Welcome To Bus Portal")
-        #         root.config(bg="Red")
-        #         root.resizable(False,False)
-
-        #         fr=Frame(root,width=800,bd=10,height=50,relief="raised")
-        #         fr.place(x=0,y=5)
-        #         frlb=Label(fr,text="Fare Portal",font=('arial',15,'bold'),width=15)
-        #         frlb.place(x=270,y=0)
-
-        #         lb=Label(root,text="Bus Route",font=('arial',15,'bold'),width=20).place(x=19,y=100)
-        #         fr=StringVar()
-        #         tx=Entry(root,font=10,textvariable=fr)
-        #         tx.place(x=300,y=100)
-        #         to=StringVar()
-        #         tx2=Entry(root,font=10,textvariable=to)
-        #         tx2.place(x=550,y=100)
-
-        #         typ=StringVar()
-
-        #         lb2=Label(root,text="Bus Type",font=('arial',15,'bold'),width=20).place(x=19,y=150)
-        #         tx2=Entry(root,font=10,textvariable=typ).place(x=300,y=150)
-
-        #         rn=StringVar()
-
-        #         lb3=Label(root,text="Route Number",font=('arial',15,'bold'),width=20).place(x=19,y=200)
-        #         tx =Entry(root,font=10,textvariable=rn).place(x=300,y=200)
-
-        #         fare=StringVar()
-        #         lb4=Label(root,text="Per Seat Fare",font=('arial',15,'bold'),width=20).place(x=19,y=250)
-        #         text1=Label(root,text="",font=('arial',15,'bold'),textvariable=fare).place(x=300,y=250)
-
-        #         nop=StringVar()
-        #         lb5=Label(root,text="Number of Passenger",font=('arial',15,'bold'),width=20).place(x=19,y=300)
-        #         text2=Label(root,text="",font=('arial',15,'bold'),textvariable=nop).place(x=300,y=300)
-
-        #         tsf=StringVar()
-        #         lb6=Label(root,text="Total Seat Fare",font=('arial',15,'bold'),width=20).place(x=19,y=350)
-        #         tx7=Entry(root,font=10,textvariable=tsf).place(x=300,y=350)
-
-        #         mc=StringVar()
-        #         lb7=Label(root,text="Meal Cost",font=('arial',15,'bold'),width=20).place(x=19,y=400)
-        #         tx8=Entry(root,font=10,textvariable=mc).place(x=300,y=400)
-
-        #         tf=StringVar()
-        #         lb8=Label(root,text="Total Fare",font=('arial',15,'bold'),width=20).place(x=19,y=450)
-        #         tx9=Entry(root,font=10,textvariable=tf).place(x=300,y=450)
-
-        #         tax=StringVar()
-        #         tax.set("18%")
-        #         lb9=Label(root,text="Tax",font=('arial',15,'bold'),width=20).place(x=19,y=500)
-        #         tx10=Entry(root,font=10,textvariable=tax).place(x=300,y=500)
-
-        #         tpf=StringVar()
-        #         lb10=Label(root,text="Total Payble For",font=('arial',15,'bold'),width=20).place(x=19,y=550)
-        #         tx10=Entry(root,font=10,textvariable=tpf).place(x=300,y=550)
-
-        #         def clearb():
-        #             qExit=messagebox.askyesno(" Quit System ", " Do you really want to cancel trip ? ")
-        #             if qExit > 0:
-        #                 root.destroy()
-        #                 return
-        #             else:
-        #                 messagebox.showinfo("Message","Moving to Fare page")
-
-        #         def tick():
-        #             os.system("ticket3.py")#pay
-
-        #         def close():
-        #             root.destroy()         #re book
-        #         btn=Button(root,text="Cancel Trip",font=('arial',10,'bold'),command=clearb,width=12,bd=8,relief="raised").place(x=50,y=650)
-        #         btn=Button(root,text="Book",command=tick,font=('arial',10,'bold'),width=12,bd=8,relief="raised").place(x=250,y=650)
-        #         btn=Button(root,text="Re-Book",command=close,font=('arial',10,'bold'),width=12,bd=8,relief="raised").place(x=450,y=650)
-
-        # -------------------------------------------------------------------------------------------------
+        
         btn4 = Button(self.root, text="Reset", font=10, bd=10,
                       relief="raised", width=15, command=res).place(x=40, y=720)
-        # btn5=Button(self.root,text="Fare",command=new,font=10,bd=10,relief="raised",width=15).place(x=285,y=720)
+        btn5=Button(self.root,text="Fare",command=self.new,font=10,bd=10,relief="raised",width=15).place(x=285,y=720)
         btn7 = Button(self.root, text="Exit", font=10, bd=10, relief="raised",
                       width=15, command=self.root.destroy).place(x=530, y=720)
     # ---------------All Funtions--------------------------------------------------------------
@@ -297,6 +224,10 @@ class customerclass:
                             self.old.set("0")
                         else:
                             self.old.set(int(row[10]))
+                        if row[18]!=None or row[18]!=" ":
+                            self.meals.set("1")
+                        else:
+                            self.meals.set(None)
                         self.feed.set(row[18])
                         return
             elif self.fnametxt.get() == "" or self.lnametxt.get() == "" or self.contact.get() == "" or self.emailtxt == "":
@@ -316,39 +247,32 @@ class customerclass:
         except Exception as ex:
             messagebox.showerror("error", ex, parent=self.root)
         con.close()
-    # def submit_all():
-    #     fro=from_From.get()
-    #     too=to_To.get()
-    #     passen=passe.get()
-    #     agea=age_adult.get()
-    #     nomeal=feed.get()
-    #     agec=child.get()
-    #     ageo=old.get()
-    #     try:
-    #         conn = pymysql.connect(host='localhost',user='root',password='123456',db='ticketing')
-    #         mydb=conn.cursor()
-    #         mydb.execute("insert into passenger(fromD,toD,nbseats,nrseats,noofmeals,childnrseats,oldnrseats) values('"+fro+"','"+too+"','"+passen+"','"+agea+"','"+nomeal+"','"+agec+"','"+ageo+"')")
-    #         conn.commit()
-    #         messagebox.showinfo("Message","Destination Submitted")
-    #     except:
-    #         conn.rollback()
-    #         messagebox.showerror("Message","Destination not Submitted")
-    #     conn.close()
-
     def submit(self):
         con = sqlite3.connect(database=r'ticketing.db')
         cur = con.cursor()
         try:
-            if self.busclass.get() == "" or self.g.get() == "":
+            if self.busclass.get() == "" or self.g.get() == "" or self.passe==0 or self.from_From.get()=="Select Destination" or self.to_To.get()=="Select Destination":
                 messagebox.showerror(
                     "Warning", "Please submit all crediantials", parent=self.root)
             else:
                 cur.execute("select * from passenger where phone=?", (self.contact.get(),))
                 row=cur.fetchone()
                 if row!=None:
-                    cur.execute("update passenger set type=?,class=?,aduseat=?, chiseat=?,oldseat=?, totseat=?, fromD=? , toD=? where phone=?",(self.g.get(),self.busclass.get(),self.adult.get(),self.child.get(),self.old.get(),self.passe,self.from_From.get(), self.to_To.get(),self.contact.get()))
-                    con.commit()
-                    messagebox.showinfo("Message","Data Submitted",parent=self.root)
+                    if self.from_From.get()==self.to_To.get():
+                        messagebox.showerror("Warning","You destination should be different!!!!",parent=self.root)
+                    else:
+                        if self.meals.get()=="1":
+                            cur.execute("update passenger set type=?,class=?,aduseat=?, chiseat=?,oldseat=?, totseat=?, fromD=? , toD=?,noofmeals=? where phone=?",(self.g.get(),self.busclass.get(),self.adult.get(),self.child.get(),self.old.get(),self.passe,self.from_From.get(), self.to_To.get(),self.feed.get(),self.contact.get()))
+                            con.commit()
+                            messagebox.showinfo("Message","Data Submitted",parent=self.root)
+                        elif self.meals.get()=="0":
+                            feed=0
+                            cur.execute("update passenger set type=?,class=?,aduseat=?, chiseat=?,oldseat=?, totseat=?, fromD=? , toD=?,noofmeals=? where phone=?",(self.g.get(),self.busclass.get(),self.adult.get(),self.child.get(),self.old.get(),self.passe,self.from_From.get(), self.to_To.get(),feed,self.contact.get()))
+                            con.commit()
+                            messagebox.showinfo("Message","Data Submitted",parent=self.root)
+                        else:
+                            messagebox.showerror("Warning","Please select meal",parent=self.root)
+
                 else:
                     messagebox.showerror("Warning","Please login first , if you login already just add you contact number.",parent=self.root)
         except Exception as ex:
@@ -360,14 +284,99 @@ class customerclass:
         self.passe=self.adult.get()+self.old.get()+self.child.get()
         # print(self.passe)
         self.tx8.config(text=f"{str(self.passe)}")
-        print(self.meals)
-        if self.meals=="Yes":
-            self.lb12 = Label(self.frame4, text="Number of Meals:",
-                            font=20, width=15).place(x=380, y=535)
-
-            self.tx5 = Entry(self.frame4, width=20, font=20,
-                            textvariable=self.feed).place(x=450, y=576)
+        if self.meals.get()=='0':
+            self.tx5.config(state=DISABLED)
+            self.lb12['state']=DISABLED
+        if self.meals.get()=='1':
+            self.tx5.config(state=NORMAL)
+            self.lb12['state']=NORMAL
         self.tx8.after(1000,self.update_content)
+# ---------------------------------------------------------------------------------
+
+        # 2nd Module
+    def new(self):
+        # if num1.get()=="" and num2.get()=="" and num3.get()=="" and num4.get()=="" and passe.get()=="" and feed.get()=="":
+        #     messagebox.showerror("Error","Check all Credentials")
+        # else:
+        #     messagebox.showinfo("Error","Welcome to Fare Page")
+
+
+        self.fareWin=Tk()
+        self.fareWin.geometry("800x750")
+        self.fareWin.title("Welcome To Bus Portal")
+        self.fareWin.config(bg="Red")
+        self.fareWin.resizable(False,False)
+
+        self.fr=Frame(self.fareWin,width=800,bd=10,height=50,relief="raised")
+        self.fr.place(x=0,y=5)
+        self.frlb=Label(self.fr,text="Fare Portal",font=('arial',15,'bold'),width=15)
+        self.frlb.place(x=270,y=0)
+
+        self.lb=Label(self.fareWin,text="Bus Route",font=('arial',15,'bold'),width=20).place(x=19,y=100)
+        self.fr=StringVar()
+        self.tx=Entry(self.fareWin,font=10,textvariable=self.fr)
+        self.tx.place(x=300,y=100)
+        self.to=StringVar()
+        self.tx2=Entry(self.fareWin,font=10,textvariable=self.to)
+        self.tx2.place(x=550,y=100)
+
+        self.typ=StringVar()
+
+        self.lb2=Label(self.fareWin,text="Bus Type",font=('arial',15,'bold'),width=20).place(x=19,y=150)
+        self.tx2=Entry(self.fareWin,font=10,textvariable=typ).place(x=300,y=150)
+
+        self.rn=StringVar()
+
+        self.lb3=Label(self.fareWin,text="Route Number",font=('arial',15,'bold'),width=20).place(x=19,y=200)
+        self.tx =Entry(self.fareWin,font=10,textvariable=rn).place(x=300,y=200)
+
+        self.fare=StringVar()
+        self.lb4=Label(self.fareWin,text="Per Seat Fare",font=('arial',15,'bold'),width=20).place(x=19,y=250)
+        self.text1=Label(self.fareWin,text="",font=('arial',15,'bold'),textvariable=fare).place(x=300,y=250)
+
+        self.nop=StringVar()
+        self.lb5=Label(self.fareWin,text="Number of Passenger",font=('arial',15,'bold'),width=20).place(x=19,y=300)
+        self.text2=Label(root,text="",font=('arial',15,'bold'),textvariable=nop).place(x=300,y=300)
+
+        self.tsf=StringVar()
+        self.lb6=Label(self.fareWin,text="Total Seat Fare",font=('arial',15,'bold'),width=20).place(x=19,y=350)
+        self.tx7=Entry(self.fareWin,font=10,textvariable=tsf).place(x=300,y=350)
+
+        self.mc=StringVar()
+        self.lb7=Label(self.fareWin,text="Meal Cost",font=('arial',15,'bold'),width=20).place(x=19,y=400)
+        self.tx8=Entry(self.fareWin,font=10,textvariable=mc).place(x=300,y=400)
+
+        self.tf=StringVar()
+        self.lb8=Label(self.fareWin,text="Total Fare",font=('arial',15,'bold'),width=20).place(x=19,y=450)
+        self.tx9=Entry(self.fareWin,font=10,textvariable=tf).place(x=300,y=450)
+
+        self.tax=StringVar()
+        self.tax.set("18%")
+        self.lb9=Label(self.fareWin,text="Tax",font=('arial',15,'bold'),width=20).place(x=19,y=500)
+        self.tx10=Entry(self.fareWin,font=10,textvariable=tax).place(x=300,y=500)
+
+        self.tpf=StringVar()
+        self.lb10=Label(self.fareWin,text="Total Payble For",font=('arial',15,'bold'),width=20).place(x=19,y=550)
+        self.tx10=Entry(self.fareWin,font=10,textvariable=tpf).place(x=300,y=550)
+        self.btn=Button(self.fareWin,text="Cancel Trip",font=('arial',10,'bold'),command=self.clearb,width=12,bd=8,relief="raised").place(x=50,y=650)
+        self.btn=Button(self.fareWin,text="Book",command=self.tick,font=('arial',10,'bold'),width=12,bd=8,relief="raised").place(x=250,y=650)
+        self.btn=Button(self.fareWin,text="Re-Book",command=self.lose,font=('arial',10,'bold'),width=12,bd=8,relief="raised").place(x=450,y=650)
+
+    def clearb(self):
+        qExit=messagebox.askyesno(" Quit System ", " Do you really want to cancel trip ? ")
+        if qExit > 0:
+            self.fareWin.destroy()
+            return
+        else:
+            messagebox.showinfo("Message","Moving to Fare page")
+
+    def tick(self):
+        os.system("ticket3.py")#pay
+
+    def close(self):
+        root.destroy()         #re book
+
+        # -------------------------------------------------------------------------------------------------
 
 
 
